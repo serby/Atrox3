3184db5a7139bdab91e2f281aa87b5d7c51cadd4
luke <luke@70721fcf-611a-4140-b243-9171d3653476>
2010-04-08 00:10:49 +0000

    Adding Edinburgh Festival to the admin and front end.
    git-svn-id: svn+ssh://izzy.wd4.clock.co.uk/var/svn/Frameworks/Atrox/branches/3.2/trunk@1422 70721fcf-611a-4140-b243-9171d3653476

--- Base/Article/Article.php
+++ Base/Article/Article.php
@@ -24,14 +24,14 @@ class ArticleControl extends TaggedDataControl {
 	var $table = "Article";
 	var $key = "Id";
 	var $sequence = "Article_Id_seq";
-	var $defaultOrder = array(array("Field" => "LiveDate", "Desc" => "true"));		
+	var $defaultOrder = array(array("Field" => "LiveDate", "Desc" => "true"));
 	var $searchFields = array("Subject", "Summary", "Body", "Tags");
 	var $imageSize = array(
 		"MinWidth" => 50,
 		"MinHeight" => 50,
 		"MaxWidth" => 1000,
 		"MaxHeight" => 1000);
-	
+
 	function init() {
 
 		$this->fieldMeta["Id"] = new FieldMeta(
@@ -59,7 +59,7 @@ class ArticleControl extends TaggedDataControl {
 
 		$this->fieldMeta["ExpiryDate"] = new FieldMeta(
 			"Expiry Date", "", FM_TYPE_DATE, null, FM_STORE_ALWAYS, true);
-		
+
 		$this->fieldMeta["ExpiryDate"]->setFormatter(CoreFactory::getDateFieldFormatter());
 		$this->fieldMeta["ExpiryDate"]->setEncoder(CoreFactory::getArrayDateTimeEncoder());
 
@@ -73,40 +73,40 @@ class ArticleControl extends TaggedDataControl {
 			"Image 1", "", FM_TYPE_BINARY, null, FM_STORE_ALWAYS, true);
 
 		$this->fieldMeta["Image1Id"]->setRelationControl(CoreFactory::getBinaryControl());
-		
+
 		$this->fieldMeta["Image1Id"]->setValidation(CoreFactory::getImageValidation(
 			$this->imageSize["MinWidth"],
 			$this->imageSize["MinHeight"],
 			$this->imageSize["MaxWidth"],
 			$this->imageSize["MaxHeight"]));
-			
+
 		$this->fieldMeta["Image2Id"] = new FieldMeta(
 			"Image 2", "", FM_TYPE_BINARY, null, FM_STORE_ALWAYS, true);
-			
+
 		$this->fieldMeta["Image2Id"]->setRelationControl(CoreFactory::getBinaryControl());
-		
+
 		$this->fieldMeta["Image2Id"]->setValidation(CoreFactory::getImageValidation(
 			$this->imageSize["MinWidth"],
 			$this->imageSize["MinHeight"],
 			$this->imageSize["MaxWidth"],
 			$this->imageSize["MaxHeight"]));
-			
+
 		$this->fieldMeta["Image3Id"] = new FieldMeta(
 			"Image 3", "", FM_TYPE_BINARY, null, FM_STORE_ALWAYS, true);
-			
+
 		$this->fieldMeta["Image3Id"]->setRelationControl(CoreFactory::getBinaryControl());
-		
+
 		$this->fieldMeta["Image3Id"]->setValidation(CoreFactory::getImageValidation(
 			$this->imageSize["MinWidth"],
 			$this->imageSize["MinHeight"],
 			$this->imageSize["MaxWidth"],
 			$this->imageSize["MaxHeight"]));
-			
+
 		$this->fieldMeta["Image4Id"] = new FieldMeta(
 			"Image 4", "", FM_TYPE_BINARY, null, FM_STORE_ALWAYS, true);
-			
+
 		$this->fieldMeta["Image4Id"]->setRelationControl(CoreFactory::getBinaryControl());
-		
+
 		$this->fieldMeta["Image4Id"]->setValidation(CoreFactory::getImageValidation(
 			$this->imageSize["MinWidth"],
 			$this->imageSize["MinHeight"],
@@ -121,24 +121,24 @@ class ArticleControl extends TaggedDataControl {
 
 		$this->fieldMeta["Attachment3Id"] = new FieldMeta(
 			"Attachment 3", "", FM_TYPE_BINARY, null, FM_STORE_ALWAYS, true);
-		
+
 		$this->fieldMeta["Attachment4Id"] = new FieldMeta(
 			"Attachment 4", "", FM_TYPE_BINARY, null, FM_STORE_ALWAYS, true);
-		
+
 		$this->fieldMeta["ExtraDate1"] = new FieldMeta(
 			"Extra Date 1", "", FM_TYPE_DATE, null, FM_STORE_ALWAYS, true);
-		
+
 		$this->fieldMeta["ExtraDate1"]->setFormatter(CoreFactory::getDateFieldFormatter());
-		
+
 		$this->fieldMeta["ExtraDate1"]->setEncoder(CoreFactory::getArrayDateEncoder());
 
 		$this->fieldMeta["Active"] = new FieldMeta(
-			"Active", "", FM_TYPE_BOOLEAN, null, FM_STORE_ALWAYS, true);	
-		
+			"Active", "t", FM_TYPE_BOOLEAN, null, FM_STORE_ALWAYS, true);
+
 		$this->fieldMeta["Tags"] = new FieldMeta(
 			"Tags", "", FM_TYPE_TAG, null, FM_STORE_ALWAYS, true);
 	}
-	
+
 	function afterInsert(&$dataEntity) {
 		parent::afterInsert($dataEntity);
 		$this->addNote($dataEntity, 1);
@@ -150,18 +150,18 @@ class ArticleControl extends TaggedDataControl {
 		$this->addNote($dataEntity, 2);
 		$this->clearSectionCache();
 	}
-	
+
 	function afterDelete(&$dataEntity) {
 		parent::afterDelete($dataEntity);
 		$this->addNote($dataEntity, 3);
 		$this->clearSectionCache();
 	}
-	
+
 	function addNote(&$dataEntity, $status) {
 		$noteControl = BaseFactory::getNoteControl();
-		$noteControl->addNote($this->type . ": " . $noteControl->status[$status] . " (Id:" . $dataEntity->get("Id") . ")", $this->type, $dataEntity->get("Id"), $status);		
+		$noteControl->addNote($this->type . ": " . $noteControl->status[$status] . " (Id:" . $dataEntity->get("Id") . ")", $this->type, $dataEntity->get("Id"), $status);
 	}
-	
+
 	function clearSectionCache() {
 		$cacheControl = CoreFactory::getCacheControl();
 		$cacheControl->deleteWebPageCache($this->type);
@@ -173,26 +173,26 @@ class ArticleControl extends TaggedDataControl {
 	 */
 	function getLatestAnnouncements($limit = 3) {
 		$this->clearFilter();
-		
+
 		if ($this->type != null) {
 			$this->retrieveByTag($this->type . "/Announcement");
 		} else {
 			$this->retrieveByTag($this->table . "/Announcement");
 		}
-		
+
 		$filter = $this->getFilter();
 		$filter->addOrder("LiveDate", true);
-		
+
 		$application = CoreFactory::getApplication();
 		$date = $application->getCurrentDateTime();
-		
+
 		$liveDateConditions[] = $filter->makeConditional($this->table, "LiveDate", $date, "<=");
-		$filter->addConditionalGroup($liveDateConditions, "AND");		
-		
+		$filter->addConditionalGroup($liveDateConditions, "AND");
+
 		$expiryDateConditions[] = $filter->makeConditional($this->table, "ExpiryDate", $date, ">=", "OR");
 		$expiryDateConditions[] = $filter->makeConditional($this->table, "ExpiryDate", null, "IS", "OR");
 		$filter->addConditionalGroup($expiryDateConditions, "AND");
-		
+
 		$filter->addLimit($limit);
 		$this->setFilter($filter);
 	}
@@ -208,7 +208,7 @@ class ArticleControl extends TaggedDataControl {
 	 */
 	function getLatest($limit = 3, $excludeIds = null, $includeTags = null, $excludeTags = null, $includeTagOperator = "AND", $excludeTagOperator = "OR", $showActive = false) {
 		$this->clearFilter();
-		
+
 		if ($includeTags == null) {
 			if ($this->type != null) {
 				$includeTags = $this->type;
@@ -216,10 +216,10 @@ class ArticleControl extends TaggedDataControl {
 				$includeTags = $this->table;
 			}
 		}
-		
+
 		$this->retrieveByTag($includeTags, $includeTagOperator, $excludeTags, $excludeTagOperator);
 		$filter = $this->getFilter();
-		
+
 		$filter->addOrder("LiveDate", true);
 
 		if ($excludeIds) {
@@ -233,25 +233,25 @@ class ArticleControl extends TaggedDataControl {
 		}
 
 		$date = $this->application->getCurrentDateTime();
-		
+
 		$liveDateConditions[] = $filter->makeConditional($this->table, "LiveDate", $date, "<=");
-		$filter->addConditionalGroup($liveDateConditions, "AND");		
-		
+		$filter->addConditionalGroup($liveDateConditions, "AND");
+
 		$expiryDateConditions[] = $filter->makeConditional($this->table, "ExpiryDate", $date, ">=", "OR");
 		$expiryDateConditions[] = $filter->makeConditional($this->table, "ExpiryDate", null, "IS", "OR");
 		$filter->addConditionalGroup($expiryDateConditions, "AND");
-		
+
 		if ($showActive) {
 			$filter->addConditional($this->table, "Active", true);
 		}
-		
+
 		if ($limit != null) {
 			$filter->addLimit($limit);
 		}
 		$this->setFilter($filter);
 	}
-	
-	
+
+
 	/*
 	 * Get live and active articles
 	 * @param Mixed $includeTags All tags to be included in the filter
