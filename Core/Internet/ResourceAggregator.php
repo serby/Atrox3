69fdd4db1513a28b66513190e4cb20d562537a80
dom <dom@70721fcf-611a-4140-b243-9171d3653476>
2010-06-25 11:59:45 +0000

    Updating phpDoc comments.
    git-svn-id: svn+ssh://izzy.wd4.clock.co.uk/var/svn/Frameworks/Atrox/branches/3.2/trunk@1564 70721fcf-611a-4140-b243-9171d3653476

--- Core/Internet/ResourceAggregator.php
+++ Core/Internet/ResourceAggregator.php
@@ -10,22 +10,53 @@
  */
 class ResourceAggregator {
 
+	/**
+	 * The path to the site root.
+	 * @var string
+	 */
 	protected $sitePath;
+
+	/**
+	 * The path to the cache location.
+	 * @var string
+	 */
 	protected $cachePath;
+
+	/**
+	 * The URL to the cache location.
+	 * @var string
+	 */
 	protected $cacheUrl;
 
 	/**
+	 * The delegate for aggregating the resources.
 	 * @var IResourceAggregatorDelegate
 	 */
 	protected $delegate;
 
 	/**
+	 * The processor to apply to the aggregated resources.
 	 * @var IProcessor
 	 */
 	protected $processor;
 
+	/**
+	 * Collection of all the resources to aggregate.
+	 * @var Array
+	 */
 	protected $resources = array();
 
+	/**
+	 * Sets up the aggregator and assigns parameters to variables.
+	 *
+	 * @author Dom Udall <dom.udall@clock.co.uk>
+	 *
+	 * @param string $sitePath
+	 * @param string $cachePath
+	 * @param string $cacheUrl
+	 * @param IResourceAggregatorDelegate $delegate
+	 * @param IProcessor $processor
+	 */
 	public function __construct($sitePath, $cachePath, $cacheUrl = null, IResourceAggregatorDelegate $delegate,
 		IProcessor $processor) {
 
@@ -37,6 +68,15 @@ class ResourceAggregator {
 		$this->processor = $processor;
 	}
 
+	/**
+	 * Adds the given resources to the specified group, using the options if provided.
+	 *
+	 * @author Dom Udall <dom.udall@clock.co.uk>
+	 *
+	 * @param array $resources Collection of resources to aggregate.
+	 * @param string $group The group to collect the resources in.
+	 * @param stdClass $options
+	 */
 	public function collect(array $resources, $group = "default", stdClass $options = null) {
 		$resourceGroup = isset($this->resources[$group]) ? $this->resources[$group] : null;
 
@@ -51,6 +91,16 @@ class ResourceAggregator {
 		}
 	}
 
+	/**
+	 * Creates a group with the specified name, and sets it with the given options.
+	 *
+	 * @author Dom Udall <dom.udall@clock.co.uk>
+	 *
+	 * @param string $name The name of the group to create.
+	 * @param stdClass $options The options for the group.
+	 *
+	 * @return stdClass The created group.
+	 */
 	protected function createGroup($name, stdClass $options) {
 		$group = new stdClass();
 		foreach ($options as $key => $value) {
@@ -61,6 +111,14 @@ class ResourceAggregator {
 		return $group;
 	}
 
+	/**
+	 * Creates an item to be aggregated.
+	 *
+	 * @author Dom Udall <dom.udall@clock.co.uk>
+	 *
+	 * @param string $type The type of item, used to determine how it is aggregated.
+	 * @param string $value The value of the item.
+	 */
 	protected function createItem($type, $value) {
 		$item = new stdClass();
 		$item->type = $type;
@@ -69,6 +127,13 @@ class ResourceAggregator {
 		return $item;
 	}
 
+	/**
+	 * Aggregates the resources and generates the HTML to include it on page.
+	 *
+	 * @author Dom Udall <dom.udall@clock.co.uk>
+	 *
+	 * @return string The generated HTML include.
+	 */
 	public function output() {
 
 		$response = "";
@@ -102,19 +167,35 @@ class ResourceAggregator {
 	}
 
 	/**
+	 * Gets the contents of a resource file and processes it with the aggregators processor.
 	 *
-	 * @param string $filePath
-	 * @return string
+	 * @author Dom Udall <dom.udall@clock.co.uk>
+	 *
+	 * @param string $filePath The path to the resource file.
+	 *
+	 * @return string The processed content of the resource file.
 	 */
 	protected function processFile($filePath) {
 		$contents = file_get_contents($filePath);
 		return $this->processor->process($contents);
 	}
 
+	/**
+	 * Starts the collection of on-page resources in the output buffer.
+	 *
+	 * @author Dom Udall <dom.udall@clock.co.uk>
+	 *
+	 * @param string $group The group to assign the collected resource to.
+	 */
 	public function start($group = "default") {
 		ob_start();
 	}
 
+	/**
+	 * Ends the collection of on-page resources, saves the output buffer and clears it.
+	 *
+	 * @author Dom Udall <dom.udall@clock.co.uk>
+	 */
 	public function end() {
 		$this->resources["default"]->items[] = $this->createItem("content", ob_get_contents());
 		ob_end_clean();
