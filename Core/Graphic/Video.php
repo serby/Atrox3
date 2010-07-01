368fc3a2cb995487d8a90750f7b28a9e39775fd6
luke <luke@70721fcf-611a-4140-b243-9171d3653476>
2010-07-01 18:08:21 +0000

    Adjusting layout of the video archive.
    git-svn-id: svn+ssh://izzy.wd4.clock.co.uk/var/svn/Frameworks/Atrox/branches/3.2/trunk@1577 70721fcf-611a-4140-b243-9171d3653476

--- Core/Graphic/Video.php
+++ Core/Graphic/Video.php
@@ -5,7 +5,7 @@
  * @copyright Clock Limited 2010
  * @version 3.2 - $Revision: 536 $ - $Date: 2008-03-04 11:47:00 +0100 (Thu, 03 Apr 2008) $
  */
- 
+
 /**
  * @author Robert Arnold {@link mailto:robert.arnold@clock.co.uk robert.arnold@clock.co.uk }
  * @copyright Clock Limited 2010
@@ -17,7 +17,7 @@ class VideoControl {
 
 	function createThumbnail($inputVideoFileName, $outputImageFileName, $offsetSeconds = "2",
 		$outputImageWidth = 0, $outputImageHeight = 0) {
-				
+
 		$commandOptions = array(
 			"-i '$inputVideoFileName'",
 			"-vcodec mjpeg",
@@ -33,31 +33,36 @@ class VideoControl {
 		}
 
 		$command = "ffmpeg " . implode(" ", $commandOptions) . " '$outputImageFileName'";
-		//echo $command; exit;
+//		echo $command; exit;
 		shell_exec($command);
 	}
-	
+
 	function saveThumbnail($video, $offsetSeconds, $outputImageWidth, $outputImageHeight) {
-		
-		// Make thumbnail	
-		$application = CoreFactory::getApplication();	
+
+		// Make thumbnail
+		$application = CoreFactory::getApplication();
 		$htmlControl = CoreFactory::getHtmlControl();
-		$inputVideoFileName = $application->registry->get("Path") . $htmlControl->getBinaryLocation($video->getRelation("BinaryId"));
-		$filename = md5(uniqid(rand(), true)) . ".jpg?v=@VERSION-NUMBER@";
+
+		$imageName = $video->getRelation("BinaryId")->get("Filename");
+		$siteFriendlyImageName = htmlentities($imageName);
+		$inputVideoFileName = $application->registry->get("Binary/Path") . "/" . $video->getRelation("BinaryId")->get("HashValue") . "/" . $imageName;
+
+//		$inputVideoFileName = $application->registry->get("Path") . $htmlControl->getBinaryLocation($video->getRelation("BinaryId"));
+		$filename = md5(uniqid(rand(), true)) . ".jpg";
 		$outputImageFileName = "/tmp/" . $filename;
-		
+
 		$this->createThumbnail($inputVideoFileName, $outputImageFileName, $offsetSeconds, $outputImageWidth, $outputImageHeight);
 
 		// Build Array
 		$thumbnailInfo = array();
 		$thumbnailInfo["Filename"] = $filename;
 		$thumbnailInfo["Type"] = mime_content_type($outputImageFileName);
-		$thumbnailInfo["Size"] = filesize($outputImageFileName);			
+		$thumbnailInfo["Size"] = filesize($outputImageFileName);
 		$thumbnailInfo["Remove"] = false;
 		$thumbnailInfo["TempName"] = $outputImageFileName;
-		
+
 		$video->setBinary("ThumbnailId", null, $thumbnailInfo);
-		$video->save();			
+		$video->save();
 	}
 
 }
